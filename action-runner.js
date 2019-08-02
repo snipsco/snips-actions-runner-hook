457c0decd84bdeb6441ab46ad1b843cf854ce7b0
CoorFun <coorfang@gmail.com>
2019-08-02 14:42:01 +0200

    Add runner hook entry

---
+++ action-runner.js
@@ -0,0 +1,28 @@
+const { spawn } = require('child_process')
+const path = require('path')
+
+const snipsActionRunnerBin = path.resolve(
+  __dirname,
+  'node_modules',
+  '.bin',
+  'snips-runner'
+)
+const snipsActionRunner = spawn(snipsActionRunnerBin, [], { encoding: 'utf-8' })
+
+snipsActionRunner.stdout.on('data', function(chunk) {
+  console.log('[actions-runner-hook]', chunk.toString())
+})
+snipsActionRunner.stderr.on('data', data => {
+  console.log('[actions-runner-hook]', data)
+})
+snipsActionRunner.on('close', function(code) {
+  console.log('[actions-runner-hook]', 'close code : ' + code)
+})
+snipsActionRunner.on('exit', code => {
+  console.log('[actions-runner-hook]', 'exit code : ' + code)
+  fs.close(fd, function(err) {
+    if (err) {
+      console.error(err)
+    }
+  })
+})
